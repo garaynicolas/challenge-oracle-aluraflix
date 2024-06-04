df1321b29baffb4ea6dcdcd37ed39237933a7ac3
garaynicolas <garay.nicolas002@gmail.com>
2024-06-04 10:33:01 -0300

    db, categoria y cardvideo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import Equipo from "./components/Equipo/Equipo";
+import { useEffect, useState } from "react";
+import Categoria from "./components/Equipo/Categoria";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import db from "./data/db.json";
 
 function App() {
-  return <>
-  <Header/>
-  <Equipo/>
-  <Footer/>
-  </>;
+  const [categorias, setCategorias] = useState([]);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    setCategorias(db.categorias);
+  }, []);
+
+  useEffect(() => {
+    setVideos(db.videos);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      {categorias.map((categoria) => (
+        <Categoria
+          key={categoria.id}
+          nombre={categoria.nombre}
+          categorias={categorias}
+          videos={videos.filter(
+            (video) => categoria.nombre === video.categoria
+          )}
+        />
+      ))}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Equipo/Categoria.jsx
+++ src/components/Equipo/Categoria.jsx
@@ -1,22 +1,21 @@
 import { useEffect } from "react";
-import styles from "./Equipo.module.css";
+import styles from "./Categoria.module.css";
 import { useState } from "react";
 import CardVideo from "../CardVideo/CardVideo";
+import db from "../../data/db.json";
 
-export default function Equipo() {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {}, []);
-
+export default function Categoria({videos,categoria,nombre}) {
+  
+  
   // const obj = {
   //     backgroundColor: hexToRgba(colorPrimario, 0.6),
   //   };
   return (
     <>
       <section className={styles.equipo}>
-        <h3 className={styles.titulo}>FRONT END</h3>
+        <h3 className={styles.titulo}>{nombre}</h3>
         <div className={styles.cards}>
-          <CardVideo />
+          {videos.map( video => <CardVideo key={video.id}/>)}   
         </div>
       </section>
     </>
