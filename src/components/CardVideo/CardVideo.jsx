44dd1e5edef16d9109ebda45dd6962918dc192d9
Nicolas Garay <garay.nicolas002@gmail.com>
2024-06-05 09:43:19 -0300

    cambios itu 2

--- src/components/CardVideo/CardVideo.jsx
+++ src/components/CardVideo/CardVideo.jsx
@@ -1,8 +1,13 @@
+import hexToRgba from "hex-to-rgba";
 import styles from "./CardVideo.module.css"
-export default function CardVideo({imagen}) {
+export default function CardVideo({imagen,colorPrimario}) {
+  const obj = {
+    backgroundColor: hexToRgba(colorPrimario, 0.6),
+    borderColor: hexToRgba(colorPrimario, 1),
+  };
   return (
     <>
-        <div className={styles.card}>
+        <div className={styles.card} style={obj}>
           <img className={styles.imagen} src={imagen} alt="" />
           <div className={styles.botones}>
             <button className={styles.boton}>Borrar</button>
--- src/components/Equipo/Categoria.jsx
+++ src/components/Equipo/Categoria.jsx
@@ -2,18 +2,20 @@ import styles from "./Categoria.module.css";
 import CardVideo from "../CardVideo/CardVideo";
 import hexToRgba from "hex-to-rgba";
 
-export default function Categoria({ videos,colorPrimario, nombre }) {
+export default function Categoria({ videos, colorPrimario, nombre }) {
   const obj = {
-      backgroundColor: hexToRgba(colorPrimario, 0.6),
-      borderColor: hexToRgba(colorPrimario, 1)
-    };
+    backgroundColor: hexToRgba(colorPrimario, 0.6),
+    borderColor: hexToRgba(colorPrimario, 1),
+  };
   return (
     <>
       <section className={styles.equipo}>
-        <h3 className={styles.titulo} style={obj}>{nombre.toUpperCase()}</h3>
+        <h3 className={styles.titulo} style={obj}>
+          {nombre.toUpperCase()}
+        </h3>
         <div className={styles.cards}>
           {videos.map((video) => (
-            <CardVideo key={video.id} imagen={video.imagen} />
+            <CardVideo key={video.id} colorPrimario={colorPrimario} imagen={video.imagen} />
           ))}
         </div>
       </section>
