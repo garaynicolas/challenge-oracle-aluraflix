02be1f59bae30cdf799527f967e3eb6e4aac5e84
garaynicolas <garay.nicolas002@gmail.com>
2024-06-11 18:00:25 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,16 @@ import ListCategorias from "./components/ListCategorias/ListCategorias";
 import { AluraProvider } from "./context/useAlura";
 import Formulario from "./pages/Formulario";
 
-
 function App() {
   return (
     <>
       <AluraProvider>
-       
         <Header />
         <Routes>
           <Route path="/" element={<ListCategorias />} />
           <Route path="/crear-video" element={<Formulario />} />
         </Routes>
         <Footer />
-    
       </AluraProvider>
     </>
   );
