da9f5c815e80f82af351800a0b1cd64a3ef7e47b
garaynicolas <garay.nicolas002@gmail.com>
2024-06-11 17:22:28 -0300

    video

--- src/components/HeaderVideo/HeaderVideo.jsx
+++ src/components/HeaderVideo/HeaderVideo.jsx
@@ -23,7 +23,6 @@ export default function HeaderVideo() {
           src={embedUrl}
           title="YouTube video player"
           frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
       </section>
